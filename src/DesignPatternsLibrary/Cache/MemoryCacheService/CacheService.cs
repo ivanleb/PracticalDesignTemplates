d894038ce875196fdabbcff3d01993a9cb364b16
ivan_leb <ivan_leb@mail.ru>
2021-11-22 00:58:52 +0100

    Cache Service pattern

---
+++ src/DesignPatternsLibrary/Cache/MemoryCacheService/CacheService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsLibrary.Cache.MemoryCacheService
+{
+    public class CacheService<TKey, TValue> : ICacheService<TKey, TValue>
+    {
+        protected readonly IMemoryCache _cache;
+        public Task<TValue> Get(TKey key)
+        {
+            if (_cache.TryGetValue(key, out TValue entry)) 
+                return Task.FromResult(entry);
+
+            return Task.FromResult(default(TValue));
+        }
+
+        public void Remove(TKey key) => _cache.Remove(key);
+        
+
+        public void Set(TKey key, TValue entry, MemoryCacheEntryOptions options = null)
+        {
+            _cache.Set(key, entry, options ?? new MemoryCacheEntryOptions());
+        }
+    }
+}
---
+++ src/DesignPatternsLibrary/Cache/MemoryCacheService/ICacheService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Threading.Tasks;
+
+namespace DesignPatternsLibrary.Cache.MemoryCacheService
+{
+    public interface ICacheService<TKey,TValue>
+    {
+        Task<TValue> Get(TKey key);
+        void Set(TKey key, TValue entry, MemoryCacheEntryOptions options = null);
+        void Remove(TKey key);
+    }
+}
